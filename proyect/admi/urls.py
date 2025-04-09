bf905b4cd3dfd5bfbefd23b796e48a403f592ec9
Yuliana <ymontenegro373@gmail.com>
2025-04-08 21:52:09 -0500

    Ajustes del proyecto

--- proyect/admi/urls.py
+++ proyect/admi/urls.py
@@ -1,3 +1,4 @@
+#URLS DEL ADMINISTRADOR
 from django.urls import path
 from .views import *
 from . import views
--- proyect/admi/views.py
+++ proyect/admi/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+# vista inicio
 def home(request):
     return render(request, 'home.html')
-
+# vista registro y login
 def registro_log(request):
     return render(request, 'log_regis.html')
-
+# vista index
 def index(request):
     return render(request, 'index.html')
-
+# vista administrador
 def adminis(request):
     return render (request, 'index_admin.html')
-
+# vista sobre nosotros
 def acercade(request):
     return render(request, 'SobreNosotros.html')
-
+# vista contacto
 def contacto(request):
     return render(request, 'Contactenos.html')
 
